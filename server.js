7d63072718c75654952f892fe8870b6580fba3e7
navya <ugginanavyasri@gmail.com>
2026-03-13 19:46:42 +0530

    vercel fix

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.post("/analyze", upload.single("resume"), async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log("Server running");
 });
