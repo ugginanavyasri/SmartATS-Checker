a1240e4d867ffd4f633b06fc7e67a527f26873de
navya <ugginanavyasri@gmail.com>
2026-03-13 21:25:43 +0530

    Updated project code

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const pdfParse = require("pdf-parse");
 const cors = require("cors");
 
 const app = express();
-app.use(express.static("public"));
 const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
@@ -43,7 +42,7 @@ app.post("/analyze", upload.single("resume"), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log("Server running on port " + PORT);
 });
